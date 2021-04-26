c715b129f91998ab35801b55935c9392c695e9c6
Desmond Rogers <desmondrogers2@gmail.com>
2021-04-26 17:47:28 -0500

    add all the tests

--- src/main/java/com/galvanize/cart/Cart.java
+++ src/main/java/com/galvanize/cart/Cart.java
@@ -1,4 +1,76 @@
 package com.galvanize.cart;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Cart {
+    ArrayList<Item> cart = new ArrayList<>();
+    HashMap<String, Integer> freqCount = new HashMap<>();
+
+    public ArrayList<Item> getCartItems() {
+        return cart;
+    }
+
+    public int addItem(Item item) {
+        cart.add(item);
+        return cart.size();
+    }
+
+    public double getTotalPrice() {
+        double total = 0;
+        for (Item i : cart) {
+            total += i.price;
+        }
+        return total;
+    }
+
+    public String itemizedList() {
+        if (cart.size() != 0) {
+            String print = "";
+            for (Item item : cart) {
+                print += "item: " + item.getName() + " price: " + item.getPrice() +
+                "\n";
+            }
+            return print;
+        } else {
+            return "Cart is empty";
+        }
+    }
+
+  public String itemQuantities() {
+  if (cart.size() == 0) return "No items";
+
+    for (Item item : cart) {
+        if (freqCount.containsKey(item.getName())) {
+            freqCount.put(item.getName(), freqCount.get(item.getName()) + 1);
+        } else {
+            freqCount.put(item.getName(), 1);
+        }
+    }
+
+    String print = "";
+    for (String item : freqCount.keySet()) {
+        print += "item: " + item + " qt: " + freqCount.get(item) + "\n";
+    }
+    return print;
+  }
+
+  public String onSaleItems() {
+      HashSet<String> uniqueOnSaleItems = new HashSet<>();
+      for (Item item : cart) {
+          if(item.isOnSale) {
+              uniqueOnSaleItems.add(item.getName());
+          }
+      }
+
+      String print = "";
+      for(String item : uniqueOnSaleItems) {
+          print += item + "\n";
+      }
+
+      if (print == "") return "No Items on Sale";
+      return print;
+  }
+
 }
--- src/main/java/com/galvanize/cart/Item.java
+++ src/main/java/com/galvanize/cart/Item.java
@@ -1,4 +1,48 @@
 package com.galvanize.cart;
 
 public class Item {
+    double price;
+    String name;
+    boolean isOnSale = false;
+
+    public Item(String name, double price, boolean isOnSale) {
+        if (price >= 0) {
+            this.price = price;
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Invalid Price");
+        }
+
+        this.isOnSale = isOnSale;
+    }
+
+    public Item(String name, double price) {
+        if (price >= 0) {
+            this.price = price;
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Invalid Price");
+        }
+
+    }
+
+    public Item (double price) {
+        if (price >= 0) {
+            this.price = price;
+        } else {
+            throw new IllegalArgumentException("Invalid Price");
+        }
+    }
+    
+    public Item() {
+        this.price = 0;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public double getPrice() {
+        return this.price;
+    }
 }
